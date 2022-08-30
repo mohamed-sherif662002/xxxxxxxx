6fa12f6db0d4de4bd5c0da56ec2484925b9315aa
mohamed-sherif662002 <110036738+mohamed-sherif662002@users.noreply.github.com>
2022-08-30 18:55:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/*let contanier_test = document.getElementById("contanier_animation_load");
+let time = 500;
+window.onload = function(){
+    let Fun_Set = setInterval(function(){
+        contanier_test.style.display = "block";
+        time = time + 500;
+    if(time === 5000){
+        function stop(){
+            clearInterval(Fun_Set);
+            contanier_test.style.display = "none";
+        }
+        stop();
+    }
+},time); 
+}
+*/
